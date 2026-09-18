b66a812a1afc7f2c4a7b77b0fb5632c89ab13a36
hakhoi1901 <242385605+hakhoi1901@users.noreply.github.com>
2026-09-18 13:38:15 +0700

    fix(analytics): update token for unopia.io domain in Cloudflare integration

--- public/cloudflare-analytics.js
+++ public/cloudflare-analytics.js
@@ -1,6 +1,6 @@
 const ANALYTICS_TOKENS = {
   'ustudy.hakhoi.io.vn': '4267fec093414ddaa7628adde13a3d30',
-  'ustudy.unopia.io.vn': '39ecd9932293460a809e9098a40d5f54',
+  'ustudy.unopia.io.vn': '6a76dd66dcf34f74ad566890043fa2db',
 };
 
 const token = ANALYTICS_TOKENS[window.location.hostname];
